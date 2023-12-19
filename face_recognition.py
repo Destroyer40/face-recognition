5ecdc0fd582bdb60b9601f6e47685900b08d0867
Shantanu  Pati <84756664+Destroyer40@users.noreply.github.com>
2023-12-19 21:38:48 +0530

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -2,109 +2,70 @@ import face_recognition
 import cv2
 import numpy as np
 
-
-# reference to webcam 
+# Initialize webcam
 video_capture = cv2.VideoCapture(0)
 
+# Load reference image and compute its face encoding
+known_image = face_recognition.load_image_file("image.jpg")
+known_face_encoding = face_recognition.face_encodings(known_image)[0]
 
-# Load  picture and recognize it.
-person_1= face_recognition.load_image_file("immage.jpg")
-print("scannig.......")
-person_1_face_encoding = face_recognition.face_encodings(person_1)[0]
+# Lists to store known face encodings and names
+known_face_encodings = [known_face_encoding]
+known_face_names = ["Your Name"]
 
-
-
-
-print("Done")
-
-
-# Create arrays of known face encodings and their names
-known_face_encodings = [person_1_face_encoding ] 
-known_face_names = ["your name" ]
-
-
-
-
-# Initialize some variables
-face_locations = [ ]
-face_encodings = [ ]
-face_names     = [ ]
-process_frame  = True
+process_frame = True
 
 while True:
-
-
-# Grab a single frame of video
+    # Capture a single frame
     ret, frame = video_capture.read()
 
-
-# Resize frame of video to 1/4 size for faster face recognition processing
+    # Resize frame for faster processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-
-# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    # Convert color space
     rgb_small_frame = small_frame[:, :, ::-1]
 
-
-# Only process every other frame of video to save time
     if process_frame:
-
-
-# Find all the faces and face encodings in the current frame of video
+        # Detect faces and compute encodings
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-        face_names     = [ ]
+        face_names = []
+
         for face_encoding in face_encodings:
-
-
-# See if the face is a match for the known face(s)
+            # Compare faces
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
-
-# Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+            
             face_names.append(name)
+    
     process_frame = not process_frame
 
-
-# Display the results
+    # Display results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-
-
-
-# Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top   *= 4
+        top *= 4
         right *= 4
-        bottom*= 4
-        left  *= 4
+        bottom *= 4
+        left *= 4
 
-
-# Draw a box around the face
+        # Draw rectangle around face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 1)
-
-
-# Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 30), (right +45, bottom), (0, 0, 0), cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom - 30), (right + 45, bottom), (0, 0, 0), cv2.FILLED)
+        
+        # Add name label
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 3, bottom - 4), font, 1.0, (0, 255, 0), 1)
 
+    # Display the resulting frame
+    cv2.imshow('Press Q to Quit', frame)
 
-# Display the resulting image
-    cv2.imshow('press Q to Quit', frame)
-
-
-# Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    if cv2.waitKey(2) & 0xFF == ord('Q'):
+    # Break the loop if 'q' or 'Q' is pressed
+    if cv2.waitKey(1) & 0xFF in (ord('q'), ord('Q')):
         break
 
-# Release handle to the webcam
+# Release resources
 video_capture.release()
-cv2.closeAllWindows()
-
- 
+cv2.destroyAllWindows()
